dc2655013c3ae795f6c243e3de029e98f0e22fbb
Vivek Diwakar <vivekdiwakar6000@gmail.com>
2024-11-29 22:20:58 +0530

    Initial commit

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -1,6 +1,58 @@
+// import React, { useState } from "react";
+// import { Link } from "react-router-dom";
+// import "../App.css"; // Link your CSS file
+
+// function MobileNavbar() {
+//   const [menuVisible, setMenuVisible] = useState(false);
+
+//   // Toggle the vertical menu visibility
+//   const toggleMenu = () => {
+//     setMenuVisible(!menuVisible);
+//   };
+
+//   return (
+//     <nav className="mobile-navbar">
+//       {/* Hamburger Icon (Visible on smaller screens) */}
+//       <div className="menu-icon" onClick={toggleMenu}>
+//         <span className="navbar-toggler-icon">☰</span>
+//       </div>
+
+//       {/* Vertical Menu (Visible when menuVisible is true) */}
+//       <div className={`vertical-menu ${menuVisible ? "show-menu" : ""}`}>
+//         <ul className="list-unstyled">
+//           <li className="nav-item">
+//             <Link to="/" className="nav-link" onClick={toggleMenu}>
+//               Home
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to="/about" className="nav-link" onClick={toggleMenu}>
+//               About
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to="/register" className="nav-link" onClick={toggleMenu}>
+//               Register
+//             </Link>
+//           </li>
+//           <li className="nav-item">
+//             <Link to="/login" className="nav-link" onClick={toggleMenu}>
+//               Login
+//             </Link>
+//           </li>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+// }
+
+// export default MobileNavbar;
+
+
+
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import '../App.css';  // Link your CSS file
+import "../App.css"; // Link your CSS file
 
 function MobileNavbar() {
   const [menuVisible, setMenuVisible] = useState(false);
@@ -12,7 +64,7 @@ function MobileNavbar() {
 
   return (
     <nav className="mobile-navbar">
-      {/* Hamburger Icon (Visible on smaller screens) */}
+      {/* Hamburger Icon (Always visible) */}
       <div className="menu-icon" onClick={toggleMenu}>
         <span className="navbar-toggler-icon">☰</span>
       </div>
@@ -21,18 +73,25 @@ function MobileNavbar() {
       <div className={`vertical-menu ${menuVisible ? "show-menu" : ""}`}>
         <ul className="list-unstyled">
           <li className="nav-item">
-            <Link to="/" className="nav-link" onClick={toggleMenu}>Home</Link>
+            <Link to="/" className="nav-link" onClick={toggleMenu}>
+              Home
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/about" className="nav-link" onClick={toggleMenu}>About</Link>
+            <Link to="/about" className="nav-link" onClick={toggleMenu}>
+              About
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/register" className="nav-link" onClick={toggleMenu}>Register</Link>
+            <Link to="/register" className="nav-link" onClick={toggleMenu}>
+              Register
+            </Link>
           </li>
           <li className="nav-item">
-            <Link to="/login" className="nav-link" onClick={toggleMenu}>Login</Link>
+            <Link to="/login" className="nav-link" onClick={toggleMenu}>
+              Login
+            </Link>
           </li>
-         
         </ul>
       </div>
     </nav>
