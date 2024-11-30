8f966f113b1066c4e2b90067400b05cb3fd1d40d
Vivek Diwakar <vivekdiwakar6000@gmail.com>
2024-11-30 09:32:22 +0530

    Initial commit

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -42,79 +42,79 @@ const Sidebar = ({ isOpen, toggleSidebar, isLoggedIn }) => {
       <div className={`sidebar ${isOpen ? "open" : ""}`}>
         <h3 style={{ margin: "30px 0px" }}>CompanyWise Questions</h3>
         <ul>
-         <li className="company-name">
-            <Link to="/topic/"  className="shiv">
+          <li className="company-name">
+            <Link to="/topic/" className="shiv" onClick={toggleSidebar}>
               Visa
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/media"  className="shiv">
+            <Link to="/topic/media" className="shiv" onClick={toggleSidebar}>
               Media.Net
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/deutsche" className="shiv">
+            <Link to="/topic/deutsche" className="shiv" onClick={toggleSidebar}>
               Deutsche
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/Microsoft"  className="shiv">
+            <Link to="/topic/Microsoft" className="shiv" onClick={toggleSidebar}>
               Microsoft
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/BNY"  className="shiv">
+            <Link to="/topic/BNY" className="shiv"onClick={toggleSidebar}>
               BNY
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/Salesforce"  className="shiv">
+            <Link to="/topic/Salesforce" className="shiv"onClick={toggleSidebar}>
               Salesforce
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/Cisco" className="shiv">
+            <Link to="/topic/Cisco" className="shiv"onClick={toggleSidebar}>
               Cisco
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/Unifyapps"  className="shiv">
+            <Link to="/topic/Unifyapps" className="shiv"onClick={toggleSidebar}>
               UnifyApps
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/Google" className="shiv">
+            <Link to="/topic/Google" className="shiv"onClick={toggleSidebar}>
               Google
             </Link>
           </li>
 
           <li className="company-name">
-            <Link to="/topic/Amazon"  className="shiv">
+            <Link to="/topic/Amazon" className="shiv"onClick={toggleSidebar}>
               Amazon
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/WellsFargo"  className="shiv">
+            <Link to="/topic/WellsFargo" className="shiv"onClick={toggleSidebar}>
               WellsFargo
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/Intuit"  className="shiv">
+            <Link to="/topic/Intuit" className="shiv"onClick={toggleSidebar}>
               Intuit
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/MasterCard"  className="shiv">
+            <Link to="/topic/MasterCard" className="shiv"onClick={toggleSidebar}>
               MasterCard
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/UiPath"  className="shiv">
+            <Link to="/topic/UiPath" className="shiv"onClick={toggleSidebar}>
               UiPath
             </Link>
           </li>
           <li className="company-name">
-            <Link to="/topic/Jlr"  className="shiv">
+            <Link to="/topic/Jlr" className="shiv"onClick={toggleSidebar}>
               Jlr
             </Link>
           </li>
